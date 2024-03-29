0bf24ac5b4b50373a3790248d40b5fd4eea99fa0
hayeong120 <jbg01200249@gmail.com>
2024-03-30 00:44:13 +0900

    0329 수업코드 - 하노이 타워

---
+++ Ch01/hanoiTower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void HanoiTower(int n, char a, char b, char c) {	// a : 출발지 변수, b : 경유지 변수, c : 도착지 변수
+	if (n == 1)
+		printf("원판 %d, %c -> %c\n", n, a, c);		// 여기서 n은 원판의 번호
+	else {
+		HanoiTower(n - 1, a, c, b);
+		printf("원판 %d, %c -> %c\n", n, a, c);
+		HanoiTower(n - 1, b, a, c);
+	}
+}
+
+int main(void) {
+	int n = 4;
+
+	HanoiTower(n, 'A', 'B', 'C');		// 원판 갯수, 출발지, 경유지 도착지
+
+	return 0;
+}
