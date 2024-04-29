7a0d2b1227d156b44f24eb17cc34e2f31dcf0600
hayeong120 <jbg01200249@gmail.com>
2024-04-29 15:03:40 +0900

    0429 수업코드

--- Ch02/linkedList1.c
+++ Ch02/linkedList1.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(void) {
 	struct node {
 		char data;
 		struct node* link;
 	};
+	struct node* head = (struct node*)malloc(sizeof(struct node));
+	struct node* p;
 	char input = ' ';
-	struct node* head, * p;
-	head = (struct node*)malloc(sizeof(struct node));
+
+	head->data = input;
+	head->link = NULL;
 	p = head;
-	p->data = input;
-	p->link = NULL;
-	printf("데이터 입력 : ");
+
+	printf("데이터 입력: ");
 	scanf_s("%c", &input, 1);
 	getchar();
 
@@ -21,11 +23,12 @@ int main(void) {
 		p = p->link;
 		p->data = input;
 		p->link = NULL;
-		printf("데이터 입력 : ");
+		printf("데이터 입력: ");
 		scanf_s("%c", &input, 1);
 		getchar();
 	}
-	printf("\n연결리스트 출력: \n");
+
+	printf("\n연결리스트 출력: ");
 	p = head->link;
 
 	while (p != NULL) {
@@ -35,6 +38,7 @@ int main(void) {
 	}
 	printf("\n");
 
-	// 동적 할당받은 만큼 메모리를 해제(free()함수 사용)하는 문장이 필요함
+	//동적 할당 받은 만큼 메모리 해제(free()함수 사용)하는 문장 필요함
+
 	return 0;
 }
