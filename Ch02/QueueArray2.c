2ebe5f7e38f5ff7314677363933ec8ea10ee3c93
hayeong120 <jbg01200249@gmail.com>
2024-05-17 10:00:00 +0900

    0527 수업코드 - Queue구조

---
+++ Ch02/QueueArray2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define SIZE 6
+
+int queue[SIZE];
+int rear = -1, front = 0;
+int cnt = 0;
+
+int add(int data) {
+	if (cnt == SIZE) {
+		printf("Queue is Full!\n");
+		return -1;
+	}
+	queue[++rear % SIZE] = data;
+	cnt++;
+	return 0;
+}
+
+int delete(void) {
+	if (cnt == 0) {
+		printf("Queue is Empty!\n");
+		return -1;
+	}
+	cnt--;
+	return queue[front++ % SIZE];
+}
+
+int main(void) {
+	add(10);
+	add(20);
+	add(30);
+	add(40);
+	add(50);
+
+	printf("%d\n", delete());
+	printf("%d\n", delete());
+	printf("%d\n", delete());
+	printf("%d\n", delete());
+	printf("%d\n", delete());
+	printf("%d\n", delete());
+
+	add(60);
+	add(70);
+	add(80);
+
+	return 0;
+}
