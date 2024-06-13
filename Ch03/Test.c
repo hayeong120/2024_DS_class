1e75b310341baf3ee79f9d726611087b8fb51f06
hayoung <jbg01200249@gmail.com>
2024-06-13 10:00:00 +0900

    0814 - 삽입 정렬

---
+++ Ch03/Test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void HanoiTower(int n, char a, char b, char c) {
+	if (n == 1) printf("원판 %d, %c -> %c\n", n, a, c);
+	else {
+		HanoiTower(n - 1, a, c, b);
+		printf("원판 %d, %c -> %c\n", n, a, c);
+		HanoiTower(n - 1, b, a, c);
+	}
+}
+
+int main(void) {
+	int n = 7;
+	HanoiTower(n, 'A', 'B', 'C');
+
+	return 0;
+}
